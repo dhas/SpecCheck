27a8218b5b6309043ef68c229ce33de161fe48b8
dhas <dhas@dhas.com>
2020-02-07 16:36:55 +0100

    yml for encoder cfg

--- config/config.py
+++ config/config.py
@@ -2,9 +2,13 @@ import yaml
 import os
 
 DRAW_YML = os.path.join(os.getcwd(),'config/draw.yml')
+ENCO_YML = os.path.join(os.getcwd(),'config/encoders.yml')
 
 def get_draw_config():	
 	draw_cfg = yaml.load(open(DRAW_YML), Loader=yaml.FullLoader)
 	if draw_cfg['img_side'] == 28:
 		draw_cfg['qd']['min_sz'] = 4
 	return draw_cfg
+
+def get_encoders_config():
+	return yaml.load(open(ENCO_YML), Loader=yaml.FullLoader)
--- prepare_encoders.py
+++ prepare_encoders.py
@@ -26,85 +26,86 @@ def training_necessary(checkpoint_path,cfg,cfg_path):
 		(cfg != pickle.load(open(cfg_path,'rb')))
 	return training_needed
 
-cfg = {
+def get_optimizer(parameters,settings):
+	if settings['optim'] == 'Adam':
+		optimizer = optim.Adam(parameters,lr = float(settings['lr']))
+		# optimizer = optim.Adam(parameters)
+	else:
+		raise Exception('Unknown optimizer')
+	return optimizer
+
+
+# cfg = {
 		
-	'NET01' : {'head'  :['4','M'],
-			   'tail'  :['4'],			   
-			   'trn'   :{
-							'optim'	: optim.RMSprop,
-							'lr'	: 1e-4,			   				
-						},
-			  },
+# 	'NET01' : {'head'  :['4','M'],
+# 			   'tail'  :['4'],			   
+# 			   'trn'   :{
+# 							'optim'	: optim.RMSprop,
+# 							'lr'	: 1e-4,			   				
+# 						},
+# 			  },
 
-	'NET02' : {'head'  :['32', 'M', '64', 'M'],
-			   'tail'  :['128'],
-			   'trn'   :{
-							'optim'	: optim.Adam,
-							'lr'	: 1e-4,			   				
-						},
-			  },
+# 	'NET02' : {'head'  :['32', 'M', '64', 'M'],
+# 			   'tail'  :['128'],
+# 			   'trn'   :{
+# 							'optim'	: optim.Adam,
+# 							'lr'	: 1e-4,			   				
+# 						},
+# 			  },
 
-	'NET03' : {'head'  :['32', 'M', '64', 'M'],
-			   'tail'  :['128', 'D#0.5'],
-			   'trn'   :{
-							'optim'	: optim.Adam,
-							'lr'	: 1e-4,			   				
-						},
-			  },
-	'NET04' : {'head'  :['32#BN', 'M', '64#BN', 'M'],
-			   'tail'  :['128'],
-			   'trn'   :{
-							'optim'	: optim.Adam,
-							'lr'	: 1e-4,			   				
-						},
-			  },
-	'NET05' : {'head'  :['32#IN', 'M', '64#IN', 'M'],
-			   'tail'  :['128'],
-			   'trn'   :{
-							'optim'	: optim.Adam,
-							'lr'	: 1e-4,			   				
-						},
-			  },
-	# 'VGG05': [64, 'M', 128, 'M'],
-	# 'VGG07': [64, 'M', 128, 'M', 256, 256, 'M'],
-	# 'VGG11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
-	# 'VGG13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
-	# 'VGG16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
-	# 'VGG19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
-}
+# 	'NET03' : {'head'  :['32', 'M', '64', 'M'],
+# 			   'tail'  :['128', 'D#0.5'],
+# 			   'trn'   :{
+# 							'optim'	: optim.Adam,
+# 							'lr'	: 1e-4,			   				
+# 						},
+# 			  },
+# 	'NET04' : {'head'  :['32#BN', 'M', '64#BN', 'M'],
+# 			   'tail'  :['128'],
+# 			   'trn'   :{
+# 							'optim'	: optim.Adam,
+# 							'lr'	: 1e-4,			   				
+# 						},
+# 			  },
+# 	'NET05' : {'head'  :['32#IN', 'M', '64#IN', 'M'],
+# 			   'tail'  :['128'],
+# 			   'trn'   :{
+# 							'optim'	: optim.Adam,
+# 							'lr'	: 1e-4,			   				
+# 						},
+# 			  },
+# 	# 'VGG05': [64, 'M', 128, 'M'],
+# 	# 'VGG07': [64, 'M', 128, 'M', 256, 256, 'M'],
+# 	# 'VGG11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+# 	# 'VGG13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+# 	# 'VGG16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+# 	# 'VGG19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+# }
 
 def main(out_dir):
 	encoders_root = out_dir/'_encoders'
 	encoders_root.mkdir(exist_ok=True)
 
 	draw_cfg = config.get_draw_config()
+	enc_cfg  = config.get_encoders_config()
 
-	dataset_dir = Path('./_datasets/qd_shapes/dataset')
+	dataset_dir = Path(draw_cfg['root'])/draw_cfg['qd']['root']/'dataset'
 	dataset_settings = read_settings(dataset_dir/'settings.json')
 
 	input_shape = (1,draw_cfg['img_side'],draw_cfg['img_side'])
 
 
-	training_settings = {
-		'epochs':1,
-		'log_interval': 5,
-		'batch_size': 32,
-	}
+	train_params = enc_cfg['train']
+
 	trans = transforms.Compose([
 		transforms.Lambda(lambda x:x.permute(2,0,1).type(torch.FloatTensor))
 	])
 	criterion = nn.CrossEntropyLoss()
 
 	device = torch.device('cuda')
-	for net_name in list(cfg.keys()):	
-		
-		train_params = {
-			'savedir'		: encoders_root/net_name,		
-			'epochs'		: 1,
-			'log_interval'	: 20,
-			'batch_size'	: 32,     
-		}
-
+	nets = enc_cfg['nets']
+	for net_name in nets:
+		train_params['savedir'] = encoders_root/net_name
 		train_params['savedir'].mkdir(exist_ok=True)
 		checkpoint_path = train_params['savedir']/('%s.tar' % net_name)
 		cfg_path        = train_params['savedir']/('%s.pkl' % net_name)
@@ -112,22 +113,22 @@ def main(out_dir):
 		
 		encoder = Encoder(net_name,
 			draw_cfg['img_side'],
-			cfg[net_name],
+			nets[net_name],
 			draw_cfg['num_classes'])
 		encoder.to(device)
 		summary(encoder,input_shape)
 
 		train_loader,val_loader = load.get_npy_train_loader(dataset_dir,
-			training_settings['batch_size'],
-			training_settings['batch_size'],
+			train_params['batch_size'],
+			train_params['batch_size'],
 			transforms=trans)	
 
-		if not training_necessary(checkpoint_path,cfg[net_name],cfg_path):
+		if not training_necessary(checkpoint_path,nets[net_name],cfg_path):
 			print('Encoder %s already trained' % net_name)
 		else:
-			print('Training encoder %s' % net_name)
-			optimizer = cfg[net_name]['trn']['optim'](encoder.parameters(),
-					lr = cfg[net_name]['trn']['lr'])
+			print('Training encoder %s' % net_name)			
+			optimizer = get_optimizer(encoder.parameters(), 
+				enc_cfg['train']['optimizer'])
 
 
 			trainer = EncoderTrainer(encoder,device,
@@ -135,14 +136,11 @@ def main(out_dir):
 					criterion,optimizer,
 					train_params)
 			trainer.fit(checkpoint_path)
-			pickle.dump(cfg[net_name],open(cfg_path,'wb'))
+			pickle.dump(nets[net_name],open(cfg_path,'wb'))
 
 	# 	print('')
-		explainer = DatasetExplainer(encoder,device,checkpoint_path)
-	# 	# loss,accuracy = explainer.evaluate(device,criterion,val_loader)
-	# 	# print('\nEvaluation on validation set: Loss {:.4f}, Accuracy {:.2f}%\n'.format(loss,accuracy))
-
-		specset_dir = Path('./_datasets/sd_shapes/dataset')
+		explainer = DatasetExplainer(encoder,device,checkpoint_path)		
+		specset_dir = Path(draw_cfg['root'])/draw_cfg['sd']['root']/'dataset'
 		spec_loader = load.get_npy_dataloader(specset_dir,
 			100,
 			transforms=trans)
@@ -155,3 +153,6 @@ def main(out_dir):
 			draw_cfg['img_side'],
 			misses_dict=misses_dict)
 		explainer.shap_explain(anns,preds,spec_loader.dataset.class_to_idx,train_params['savedir'])
+
+if __name__ == '__main__':
+	main(Path('./_outputs'))
